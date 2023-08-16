32669ddc669692b10b5615565711c4d50f0b2e34
MA <46489918+yungenma@users.noreply.github.com>
2023-08-16 21:00:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,77 +1,98 @@
-#!/bin/bashlsmod
-echo ----1.Set Timezone to Asia/Taipei----
+#!/bin/bash
+
+# Display a separator line
+echo "----------------------------------------"
+
+# 1. Set Timezone to Asia/Taipei
+echo "Setting Timezone to Asia/Taipei..."
 sudo timedatectl set-timezone Asia/Taipei
-#確認時區 timedatectl
-ehco ----------SetTimezone Finish----------
+echo "Timezone set to Asia/Taipei."
+echo "----------------------------------------"
 
-echo ----2.APT update / upgrade----
-sudo apt-get update -y && apt-get upgrade -y
-ehco ----------APT Update Finish----------
+# 2. APT update / upgrade
+echo "Updating and upgrading APT packages..."
+sudo apt-get update -y && sudo apt-get upgrade -y
+echo "APT packages updated and upgraded."
+echo "----------------------------------------"
 
-echo ----3.Install vmWare Tools-----
-sudo apt-get install open-vm-tools -y && apt-get install open-vm-tools-desktop -y
-ehco ----------Install vmWare Tools Finish----------
+# 3. Install vmWare Tools
+echo "Installing vmWare Tools..."
+sudo apt-get install open-vm-tools -y && sudo apt-get install open-vm-tools-desktop -y
+echo "vmWare Tools installed."
+echo "----------------------------------------"
 
-echo ----4.Install net-tools Package----
+# 4. Install net-tools Package
+echo "Installing net-tools package..."
 sudo apt-get install net-tools -y
-ehco ----------Install net-tools Finish----------
+echo "net-tools package installed."
+echo "----------------------------------------"
 
-echo ----5.Install SSH service----
+# 5. Install SSH service
+echo "Installing SSH service..."
 sudo apt-get install ssh -y
-sudo ./etc/init.d/ssh restart
-#config File > vi /etc/ssh/sshd_config
-#Check service ssh status
-ehco ----------Install SSH Service Finish----------
+sudo systemctl restart ssh
+echo "SSH service installed and restarted."
+echo "----------------------------------------"
 
-
-echo ----6.Install Brave Broswer----
-#參考這邊https://brave.com/linux/
-sudo apt install apt-transport-https curl -y
+# 6. Install Brave Browser
+echo "Installing Brave Browser..."
+sudo apt-get install apt-transport-https curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser -y
-echo ----------Install Brave Broswer Finish----------
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt-get update
+sudo apt-get install brave-browser -y
+echo "Brave Browser installed."
+echo "----------------------------------------"
 
-echo ----7.Install HTOP----
+# 7. Install HTOP
+echo "Installing HTOP..."
 sudo apt-get install htop -y
-echo ----------Install HTOP Finish----------
+echo "HTOP installed."
+echo "----------------------------------------"
 
-echo ----8.Install Wget ----
+# 8. Install Wget
+echo "Installing Wget..."
 sudo apt-get install wget -y
-echo ----------Install Wget Finish----------
+echo "Wget installed."
+echo "----------------------------------------"
 
-echo ----9.Install AnyDesk ----
-sudo wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
-sudo echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
-sudo apt update
-sudo apt install anydesk -y
-#Web link = http://deb.anydesk.com/howto.html
-echo ----------Install Anydesk Finish----------
+# 9. Install AnyDesk
+echo "Installing AnyDesk..."
+sudo wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
+echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
+sudo apt-get update
+sudo apt-get install anydesk -y
+echo "AnyDesk installed."
+echo "----------------------------------------"
 
-echo ----10.Install VS Code ----
+# 10. Install VS Code
+echo "Installing VS Code..."
 sudo apt-get install gpg -y
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
-sudo apt update
-sudo apt install code
-echo ----------Install VS Code Finish----------
+sudo apt-get update
+sudo apt-get install code -y
+echo "VS Code installed."
+echo "----------------------------------------"
 
-echo ----11.Install VLC ----
-sudo apt install vlc -y
-echo ----------Install VLC Finish----------
+# 11. Install VLC
+echo "Installing VLC..."
+sudo apt-get install vlc -y
+echo "VLC installed."
+echo "----------------------------------------"
 
-echo ----12.Install Google Chrome ----
-sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add –
-sudo gpedit /etc/apt/sources.list.d/google.list
-sudo deb http://dl.google.com/linux/chrome/deb/ stable main
-sudo apt update
+# 12. Install Google Chrome
+echo "Installing Google Chrome..."
+sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update
 sudo apt-get install google-chrome-stable -y
-echo ----------Install Google Chrome Finish----------
+echo "Google Chrome installed."
+echo "----------------------------------------"
 
-
-echo ----OS Will Reboot----
+# Reboot the OS
+echo "Rebooting the OS in 5 seconds..."
 sleep 5
 sudo reboot
